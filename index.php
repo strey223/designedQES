359c058eea0c94022a27a5641e8629f1715a8030
Grigo <grigorii.taranenko@gmail.com>
2021-01-28 16:52:54 +0300

    Hello world

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$desktop = $_GET['desktop'] ?? 'one';
+
+switch ($desktop) {
+    case 'one':
+        include './html/desktop1.html';
+        break;
+    case 'two':
+        include './html/desktop2.html';
+        break;
+    default:
+        include './html/desktop1.html';
+}
